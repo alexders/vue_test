500bba3bb6622475c5384b5899cf67d9311775c5
td_code <1098691502@qq.com>
2021-09-11 23:44:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Descripttion:
+ * @version:
+ * @Author: sueRimn
+ * @Date: 2021-09-11 23:19:19
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-09-11 23:22:23
+-->
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+//引入组件
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
+
+<style>
+</style>
